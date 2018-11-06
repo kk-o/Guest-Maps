5f4583f8a4c87fb880e3cea38ecd38d5415fc595
Uldarico Estrada <uldarico@outlook.com>
2018-11-06 12:59:13 -0800

    Added React leaflet wrapper and created map component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+
+import {
+  Map,
+  TileLayer,
+  Marker,
+  Popup
+} from 'react-leaflet';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
+
   render() {
+    const position = [this.state.lat, this.state.lng];
+
     return (
-      <div className="App">
-        <h1>Hello world! 🗺</h1>
-      </div>
+    <Map center = { position } zoom = { this.state.zoom }>
+      <TileLayer url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution = "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" 
+      />
+      <Marker position = { position } >
+        <Popup>
+          A pretty CSS3 popup. <br/> Easily customizable. 
+        </Popup> 
+      </Marker> 
+    </Map>
     );
   }
 }
